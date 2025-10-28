9a1694dcfcc58a7cd4690395c0150f92f11bee1f
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-10-28 18:01:01 +0200

    Add files via upload

---
+++ dati.py
@@ -0,0 +1,13 @@
+datumi_vardi = {
+    "01.01": {"vārdi": ["Laimnesis", "Solvita"]},
+    "02.01": {"vārdi": ["Everita", "Aiva"]},
+    "03.01": {"vārdi": ["Genoveva", "Gunita"]},
+    "04.01": {"vārdi": ["Spodra", "Ilga"]},
+    "05.01": {"vārdi": ["Zigurds", "Sigurds"]},
+    "06.01": {"vārdi": ["Kaspars", "Gaspars", "Melhiors", "Baltazars"]},
+    "07.01": {"vārdi": ["Rota", "Zigmārs"]},
+}
+
+diena = "06.01"
+
+print("Šodien vārda dienu svin:", ", ".join(datumi_vardi[diena]["vārdi"]))
